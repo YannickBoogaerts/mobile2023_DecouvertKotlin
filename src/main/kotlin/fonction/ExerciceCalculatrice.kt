d3f6c5b47ca325fcd6f93582a8d78a262d6e9fff
Yannick boogaerts <yboogaerts@gmail.com>
2023-05-10 11:47:49 +0200

    exercice Calculatrice

--- src/main/kotlin/fonction/ExerciceCalculatrice.kt
+++ src/main/kotlin/fonction/ExerciceCalculatrice.kt
@@ -5,7 +5,7 @@ import java.util.Scanner
 fun <T> saisie(message: String, parser: (String) -> T?): T {
     val scan = Scanner(System.`in`)
     print(message)
-    var retour:T? = parser(scan.nextLine())
+    var retour: T? = parser(scan.nextLine())
     while (retour == null) {
         print("entrée non valide - $message")
         retour = parser(scan.nextLine())
@@ -13,12 +13,16 @@ fun <T> saisie(message: String, parser: (String) -> T?): T {
     return retour
 }
 
+fun addition(a: Int, b: Int): Int {
+    return a + b
+}
+
 fun op(texte: String): ((Int, Int) -> Int)? =
     if (texte.length == 0)
         null
     else
         when (texte[0]) {
-            '+' -> { a, b -> a + b }
+            '+' -> ::addition
             '-' -> { a, b -> a - b }
             '*' -> { a, b -> a * b }
             '/' -> { a, b -> a / b }
@@ -26,7 +30,7 @@ fun op(texte: String): ((Int, Int) -> Int)? =
         }
 
 fun op1(texte: String): ((Int, Int) -> Int)? {
-    var retour :((Int, Int) -> Int)?
+    var retour: ((Int, Int) -> Int)?
     if (texte.length != 1)
         retour = null
     else
@@ -40,6 +44,49 @@ fun op1(texte: String): ((Int, Int) -> Int)? {
     return retour
 }
 
+fun op2(texte: String): ((Int, Int) -> Int)? {
+    var retour: ((Int, Int) -> Int)?
+    if (texte.length != 1)
+        retour = null
+    else
+        retour = when (texte[0]) {
+            '+' -> { a, b -> a + b }
+            '-' -> { a, b -> a - b }
+            '*' -> { a, b -> a * b }
+            '/' -> { a, b -> a / b }
+            else -> null
+        }
+    return retour
+}
+
+fun op3(texte: String): ((Int, Int) -> Int)? {
+    var retour: ((Int, Int) -> Int)? =
+        if (texte.length != 1)
+            null
+        else
+            when (texte[0]) {
+                '+' -> { a, b -> a + b }
+                '-' -> { a, b -> a - b }
+                '*' -> { a, b -> a * b }
+                '/' -> { a, b -> a / b }
+                else -> null
+            }
+    return retour
+}
+
+fun op4(texte: String): ((Int, Int) -> Int)? {
+    return if (texte.length != 1)
+        null
+    else
+        when (texte[0]) {
+            '+' -> { a, b -> a + b }
+            '-' -> { a, b -> a - b }
+            '*' -> { a, b -> a * b }
+            '/' -> { a, b -> a / b }
+            else -> null
+        }
+}
+
 
 fun main() {
     println(
@@ -48,4 +95,12 @@ fun main() {
             saisie("deuxieme Nombre: ") { s -> s.toIntOrNull() }
         )
     )
+}
+
+fun main1() {
+    val saisieOp = saisie("opération (+, -, *, /): ", ::op)
+    val saisieNb1 = saisie("premier Nombre: ") { s -> s.toIntOrNull() }
+    val saisieNb2 = saisie("deuxieme Nombre: ") { s -> s.toIntOrNull() }
+    val result = saisieOp(saisieNb1, saisieNb2)
+    println(result)
 }
